706f91e06efa30c1dc4804c1be941d66ab45133d
Kendra Nixon <knixon000@nyc.yearup.org>
2018-10-30 15:08:34 -0400

    completed loopsgit add main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 var prices = [33,26,99,120,12,45];
 var sum = 0;
-
 for (var i=0; i < prices.length; i++){
   sum += prices[i];
 }
-
 console.log("The sum is:", sum);
 
-var cw = "crows on the wall";
-var num = 99;
-
-for (var i=1; i > 100; i--){
-
+for (i = 99; i > 1; i--){
+  console.log(i + " crows on the wall.", i + " crows.") + console.log("1 fell down and became a wight.") + console.log(i - [1] + " crows on the wall.")
+  if (i == 2)
+    console.log(i - [1] + " crow on the wall.", i - [1] + " single crow.") + console.log("It fell down and became a wight.") + console.log("There's no one left to defend Westeros now.")
 }
-
-console.log(i + " crows on the wall")
