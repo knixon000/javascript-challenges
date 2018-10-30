11d85319a55904703724b1108fc4922ef6f7f39b
Kendra Nixon <knixon000@nyc.yearup.org>
2018-10-30 13:50:28 -0400

    fixed???

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var prices = [33,26,99,120,12,45];
 var sum = 0;
 
 for (var i=0; i < prices.length; i++){
-  sum + prices[i]
-  console.log(sum);
+  sum += prices[i];
 }
+
+console.log("The sum is:", sum);
